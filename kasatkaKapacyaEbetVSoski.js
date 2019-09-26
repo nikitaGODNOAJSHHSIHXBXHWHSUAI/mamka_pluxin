d7d72849effe5acc4b172e08f70dd36eaf90bc2d
nikitaGODNOAJSHHSIHXBXHWHSUAI <51289315+nikitaGODNOAJSHHSIHXBXHWHSUAI@users.noreply.github.com>
2019-09-26 12:43:15 +0300

    Add files via upload

---
+++ kasatkaKapacyaEbetVSoski.js
@@ -0,0 +1,17 @@
+var sasakMalikGameForwardDrive = {
+   siski: {
+      siska1: null,
+      siska2: null
+   },
+   soski: {
+      sosik1: null,
+      sosik2: null
+   },
+   analniyCherkash: {
+      onil: null
+   },
+   initUiZadniy: function(){
+   }
+};
+
+saskMalikGameForwardDrive.initUiZadniy();
